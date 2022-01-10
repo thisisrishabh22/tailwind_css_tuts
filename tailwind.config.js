5bbc164d9a9ee957e32ab7ff2cf6abf193f89b45
Rishabh Singh <ris8645208@gmail.com>
2022-01-10 14:23:24 +0530

    feat: add custom font nunito & tested flexbox

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        body: ["Nunito"],
+      },
       colors: {
         primary: "#FF6363",
         secondary: {
